9a3bce3585e56e10bd1e99e4270cf6c158ab459f
Salma-Gader <salmagader90@gmail.com>
2026-07-18 17:07:25 +0100

    fix

--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 set -e
 
+# Create required Laravel directories
+mkdir -p storage/framework/cache/data
+mkdir -p storage/framework/sessions
+mkdir -p storage/framework/views
+mkdir -p storage/logs
+mkdir -p bootstrap/cache
+
+# Permissions
+chown -R www-data:www-data storage bootstrap/cache
+chmod -R ug+rw storage bootstrap/cache
+
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
